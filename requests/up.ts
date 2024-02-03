ed6d5e9c7e476b92f981133f52063a5d77437df8
Hartaithan <hartaithan@gmail.com>
2024-02-03 14:47:37 +0900

    group posts requests and run in parallel

--- requests/up.ts
+++ requests/up.ts
@@ -3,6 +3,22 @@ import { getAuthHeaders } from "@/lib/headers";
 import type { Post } from "@/models/PostModel";
 import type { PostsUpStatus } from "@/models/UpModel";
 
+export const postUp = async (cookies: string, post: Post, index: number) => {
+  const headers = getAuthHeaders(cookies);
+  await fetch(
+    `${API_URL}/v3/status?action=deactivate&postId=${post.id}&reason_code=310`,
+    {
+      headers,
+      cache: "no-cache",
+    },
+  );
+  await fetch(`${API_URL}/v3/status?action=activate&postId=${post.id}`, {
+    headers,
+    cache: "no-cache",
+  });
+  console.info(`[POSTS_UP_${index + 1}]: post ${post.id} up complete`);
+};
+
 export const postsUp = async (
   posts: Post[],
   cookies: string,
@@ -12,29 +28,15 @@ export const postsUp = async (
     return "up_empty";
   }
 
-  const headers = getAuthHeaders(cookies);
+  const requests = [];
+  for (let index = 0; index < posts.length; index++) {
+    const post = posts[index];
+    requests.push(postUp(cookies, post, index));
+  }
 
   try {
-    console.info("[POSTS_UP]: starting");
-    for (let i = 0; i < posts.length; i++) {
-      const post = posts[i];
-      const index = i + 1;
-
-      await fetch(
-        `${API_URL}/v3/status?action=deactivate&postId=${post.id}&reason_code=310`,
-        {
-          headers,
-          cache: "no-cache",
-        },
-      );
-      console.info(`[POSTS_UP_${index}]: deactivate ${post.id} complete`);
-
-      await fetch(`${API_URL}/v3/status?action=activate&postId=${post.id}`, {
-        headers,
-        cache: "no-cache",
-      });
-      console.info(`[POSTS_UP_${index}]: activate ${post.id} complete`);
-    }
+    console.info("[POSTS_UP]: request");
+    await Promise.all(requests);
   } catch (error) {
     console.error("[POSTS_UP]: error", error);
     return "up_error";
